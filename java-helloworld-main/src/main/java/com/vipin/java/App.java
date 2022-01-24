bc77a097784decb9136109849166badbb6e599d9
Vipin Kumar Rajput <vipin.r@hcl.com>
2022-01-24 14:33:37 -0800

    Update App.java

--- java-helloworld-main/src/main/java/com/vipin/java/App.java
+++ java-helloworld-main/src/main/java/com/vipin/java/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        String password = "this is my password";
+        System.out.println( "Password is!= " + password);
         System.out.println( "Hello World!" );
     }
 }
